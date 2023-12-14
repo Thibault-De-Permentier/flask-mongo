149df000b352052b221ece7f6037705f4dd28062
Thibault-De-Permentier <dpthi04860@gmail.com>
2023-12-14 20:59:38 +0100

    ajout du update et deplacement sur une banch

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from datetime import datetime
+import ast
 
 app = Flask(__name__)
 
@@ -29,10 +30,13 @@ def create():
     dateDeSortie = request.form['dateDeSortie']
     figurines.insert_one({'nom': nom,
                           'prix': float(prix),
-                          'reference.serie':serie,
-                          'reference.personnages':personnages,
+                          'reference':{
+                            'serie':serie,
+                            'personnages':personnages
+                            },
                           'taille':int(taille),
                           'matiere':matiere,
+                          'editeur':editeur,
                           'nbEnStock':int(nbEnStock),
                           'dateDeSortie': datetime.strptime(dateDeSortie, "%Y-%m-%d")})
     return redirect(url_for('index'))
@@ -43,8 +47,35 @@ def read(id):
     return render_template('read.html', figurine=figurine)
 
 
-@app.route("/<id>/update", methods=['POST','PATCH','PUT'])
+@app.route("/<id>/update", methods=['GET'])
 def update(id):
+    figurine = figurines.find({"_id": ObjectId(id)})
+    return render_template('update.html', figurine=figurine)
+
+@app.route("/<id>/commitUpdate", methods=['POST','PUT','PATCH'])
+def commitUpdate(id):
+    nom = request.form['nom'] 
+    prix = request.form['prix']
+    serie = request.form['serie']
+    personnages = request.form['personnages']
+    taille = request.form['taille']
+    matiere = request.form['matiere']
+    editeur = request.form['editeur']
+    nbEnStock = request.form['nbEnStock']
+    dateDeSortie = request.form['dateDeSortie']
+    filter = { '_id': ObjectId(id) }
+    newvalues = { "$set": { 'nom': nom,
+                            'prix': float(prix),
+                            'reference':{
+                                'serie':serie,
+                                'personnages':ast.literal_eval(personnages)
+                            },
+                            'taille':int(taille),
+                            'matiere':matiere,
+                            'editeur':editeur,
+                            'nbEnStock':int(nbEnStock),
+                            'dateDeSortie': datetime.strptime(dateDeSortie, "%Y-%m-%d")} }
+    figurines.update_one(filter, newvalues)
     return redirect(url_for('index'))
 
 @app.route("/<id>/delete", methods=['POST','DELETE'])
